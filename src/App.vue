884c2354b7b6bb1a09079c293eed153e47660d69
princehash90 <hashim.muridi@go.minneapolis.edu>
2024-11-24 17:06:16 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import WouldYouRather from './components/WouldYouRather.vue'
+import {ref} from 'vue'
+
+const wyrQuestion = ref('Would you rather be a famous inventor or a famous writer?')
+const wyrAnswer1= ref('Famous Inventor')// reactive data
+const wyrAnswer2= ref('Famous Writer')
+
+const userSelection = ref('')// this will save the user's answer so we set to empty for now
+
+function updateUserSelection(userChoice){
+  userSelection.value = `Thanks! you chose ${userChoice}`// updating here whatever the user has made a choice.
+}
+
+</script>
+
+<template>
+  <div id="app-component">
+      <h1>Would You Rather?</h1>
+      <WouldYouRather v-bind:question="wyrQuestion"
+                      v-bind:answer1="wyrAnswer1"
+                      v-bind:answer2="wyrAnswer2"
+                      v-on:answer-selected="updateUserSelection"
+      ></WouldYouRather>
+
+      <p>{{userSelection}}</p>
+
+  </div>
+
+</template>
+
+<style scoped>
+p{
+  font-family: "cursive";
+}
+#app-component{
+  background-color: coral;
+  padding: 40px;
+}
+
+</style>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { ref, watch} from 'vue'
+
+defineProps({
+  question:String,
+  answer1:String,
+  answer2:String,
+
+})
+
+const emit =  defineEmits(['' +
+'answer-selected'
+])
+
+const choice= ref('')
+
+// function choiceMade (){
+//   //emit event to tell parent that user has made a choice
+//   emit('answer-selected', choice.value)
+// }
+// adding a watcher to watch the choice variable, when it changes, emit the event the parent that the user has made a choice
+//a different way of implementing the same functionality
+watch(choice,()=>{
+  emit('answer-selected', choice.value)
+})
+
+
+</script>
+
+<template>
+
+  <div id="wyr">
+
+    <h2>Make your Choice</h2>
+
+   <p> {{question}}</p>
+
+    <!-- this value attribute is ithe HTML value, for what data input stores-->
+    <div>
+      <!--<input v-model="choice" v-bind:value="answer1" type="radio" id="answer-1" v-on:change="choiceMade">-->
+      <!--another way to do it-->
+      <input v-model="choice" v-bind:value="answer1" type="radio" id="answer-1">
+      <label for="answer-1">{{answer1}}</label>
+      <input v-model="choice" v-bind:value="answer2" type="radio" id="answer-2">
+      <label for="answer-2">{{answer2}}</label>
+    </div>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+#wyr{
+  background-color: #242424;
+}
+p{
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+</style>
